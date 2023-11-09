5a22303f820d55ff63f18d06eb3c773d64006307
Marko <marko.milosevic.offic@gmail.com>
2023-11-10 00:46:03 +0100

    DbConnectionForm-designed

--- src/form/DBConnectionForm.java
+++ src/form/DBConnectionForm.java
@@ -145,7 +145,6 @@ public class DBConnectionForm extends javax.swing.JFrame {
      * brisem main metodu jer koristim drugu u main.Start.java
      */
 
-
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btnCancel;
     private javax.swing.JButton btnConnect;
